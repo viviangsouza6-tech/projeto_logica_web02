d3661244313ec6a9ec70433af23341909a89eca2
Vivian <Vivian.g.souza6@alunosenai.br>
2026-06-15 15:59:52 -0300

    Modificado o arquivo js/scripts_questao02.js

---
+++ js/script_questa02.js
@@ -0,0 +1,21 @@
+//PEGANDO ELEMENTOS DO DOM
+const formDados = document.querySelector('#formulario')
+const divResultado = document.querySelector('#div-dados')
+
+//CAPTURANDO O EVENTO SUBMIT [X] FORMULÁRIO
+formDados.addEventListener ('submit', (evt)=>{
+    evt.preventDefault()
+
+    
+    const objFormDados =new FormData(formDados)
+    
+    let largura = objFormDados.get('largura')
+    let altura = objFormDados.get('altura')
+
+    let area = largura * altura
+
+    divResultado.innerHTML = `A área a ser pintada é de ${area.toFixed(2).replace('.',',')}m², Total de litros para pintar essa área é de ${parseFloat(area /2).toFixed(2).replace('.',',')}l`
+
+    formDados.reset()
+    
+})
